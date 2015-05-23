da4365375df3e2d2943d9eff8db2b24687066b1a
dev.jerry <dev.jy1215jy@gmail.com>
2015-05-23 01:46:53 -0700

    allapy module

---
+++ app/code/local/AllPay/Alipay/Block/Redirect.php
@@ -0,0 +1,82 @@
+<?php
+require_once(Mage::getBaseDir('app') . '/code/local/AllPay/AllPay.Payment.Integration.php');
+
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   AllPay
+ * @package    AllPay_AllInOne
+ * @copyright  Copyright (c) 2010 AllPay (http://www.allpay.com.tw)
+ */
+class AllPay_Alipay_Block_Redirect extends Mage_Core_Block_Template {
+
+    private function _getSession() {
+        return Mage::getSingleton('checkout/session');
+    }
+
+    private function _getOrder() {
+        if ($this->getOrder()) {
+            return $this->getOrder();
+        } elseif ($orderIncrementId = $this->_getSession()->getLastRealOrderId()) {
+            return Mage::getModel('sales/order')->loadByIncrementId($orderIncrementId);
+        } else {
+            return null;
+        }
+    }
+
+    private function _getConfigData($keyword) {
+        return Mage::getStoreConfig('payment/alipay/' . $keyword, null);
+    }
+
+    protected function AutoSubmit() {
+        $oOrder = $this->_getOrder();
+        $oBilling = $oOrder->getBillingAddress();
+        $szHtml = '';
+
+        if ($oOrder) {
+            try {
+                $oPayment = new AllInOne();
+                $oPayment->ServiceURL = ($this->_getConfigData('test_mode') ? 'http://payment-stage.allpay.com.tw/Cashier/AioCheckOut' : 'https://payment.allpay.com.tw/Cashier/AioCheckOut');
+                $oPayment->HashKey = $this->_getConfigData('hash_key');
+                $oPayment->HashIV = $this->_getConfigData('hash_iv');
+                $oPayment->MerchantID = $this->_getConfigData('merchant_id');
+
+                $oPayment->Send['ReturnURL'] = Mage::getUrl('alipay/processing/response');
+                $oPayment->Send['ClientBackURL'] = Mage::getUrl('');
+                $oPayment->Send['OrderResultURL'] = Mage::getUrl('alipay/processing/result');
+                $oPayment->Send['MerchantTradeNo'] = ($this->_getConfigData('test_mode') ? $this->_getConfigData('test_order_prefix') : '') . $oOrder->getIncrementId();
+                $oPayment->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');
+                $oPayment->Send['TotalAmount'] = (int) $oOrder->getGrandTotal();
+                $oPayment->Send['TradeDesc'] = "AllPay_Magento_Module";
+                $oPayment->Send['ChoosePayment'] = PaymentMethod::Alipay;
+                $oPayment->Send['Remark'] = '';
+                $oPayment->Send['ChooseSubPayment'] = PaymentMethodItem::None;
+                $oPayment->Send['NeedExtraPaidInfo'] = ExtraPaymentInfo::No;
+                $oPayment->Send['DeviceSource'] = DeviceType::PC;
+
+                array_push($oPayment->Send['Items'], array('Name' => Mage::helper('alipay')->__('Commodity Group'), 'Price' => (int) $oOrder->getGrandTotal(), 'Currency' => Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol(), 'Quantity' => 1, 'URL' => ''));
+
+                $oPayment->SendExtend['Email'] = $oBilling->getEmail();
+                $oPayment->SendExtend['PhoneNo'] = $oBilling->getTelephone();
+                $oPayment->SendExtend['UserName'] = $oBilling->getName();
+
+                $szHtml = $oPayment->CheckOutString();
+            } catch (Exception $e) {
+                Mage::throwException($e->getMessage());
+            }
+        }
+
+        return $szHtml;
+    }
+
+}
---
+++ app/code/local/AllPay/BARCODE/controllers/ProcessingController.php
@@ -0,0 +1,214 @@
+<?php
+require_once(Mage::getBaseDir('app') . '/code/local/AllPay/AllPay.Payment.Integration.php');
+
+/**
+ * ProcessingController short summary.
+ *
+ * ProcessingController description.
+ *
+ * @version 1.0
+ * @author andy.chao
+ * @version 1.1
+ * @author shawn.chang
+ */
+class AllPay_BARCODE_ProcessingController extends Mage_Core_Controller_Front_Action {
+
+		private function _getCheckout() {
+				return $this->_getSession();
+		}
+
+    private function _getOrder($orderID = NULL) {
+        if (!isset($orderID)) {
+            $orderID = $this->_getSession()->getLastRealOrderId();
+        }
+        if ($orderID) {
+            return Mage::getModel('sales/order')->loadByIncrementId($orderID);
+        } else {
+            return null;
+        }
+    }
+
+    private function _getSession() {
+        return Mage::getSingleton('checkout/session');
+    }
+
+    private function _getConfigData($keyword) {
+        return Mage::getStoreConfig('payment/barcode/' . $keyword, null);
+    }
+
+    /**
+     * main entry point
+     */
+    public function viewAction() {
+        
+    }
+
+    /**
+     * when customer selects AllPay payment method
+     */
+    public function redirectAction() {
+        try {
+            $oSession = $this->_getSession();
+            $oOrder = $this->_getOrder();
+
+            if (!$oOrder->getId()) {
+                Mage::throwException(Mage::helper('barcode')->__('Order No not found'));
+            }
+            if ($oOrder->getState() != Mage_Sales_Model_Order::STATE_NEW) {
+                $oOrder->setState(
+                        Mage_Sales_Model_Order::STATE_NEW, Mage_Sales_Model_Order::STATE_NEW, Mage::helper('barcode')->__('Redirect to pay page')
+                )->save();
+
+                $oOrder->sendNewOrderEmail();  //發出E-mail通知信
+                $oOrder->setEmailSent(true);
+            }
+
+            if ($oSession->getQuoteId() && $oSession->getLastSuccessQuoteId()) {
+                $oSession->setAllpayQuoteId($oSession->getQuoteId());
+                $oSession->setAllpaySuccessQuoteId($oSession->getLastSuccessQuoteId());
+                $oSession->setAllpayRealOrderId($oSession->getLastRealOrderId());
+                $oSession->getQuote()->setIsActive(false)->save();
+                $oSession->clear();
+            }
+
+            $this->loadLayout();
+            $this->renderLayout();
+            return;
+        } catch (Mage_Core_Exception $e) {
+            $this->_getCheckout()->addError($e->getMessage());
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+        $this->_redirect('checkout/cart');
+    }
+
+    /**
+     * AllPay returns POST variables to this action
+     */
+    public function responseAction() {
+        try {
+            $oPayment = new AllInOne();
+            $oPayment->HashKey = $this->_getConfigData('hash_key');
+            $oPayment->HashIV = $this->_getConfigData('hash_iv');
+            $oPayment->MerchantID = $this->_getConfigData('merchant_id');
+
+            $arFeedback = $oPayment->CheckOutFeedback();
+
+            $this->_processSale($arFeedback);
+
+            print '1|OK';
+        } catch (Mage_Core_Exception $e) {
+            $this->_getCheckout()->addError($e->getMessage());
+            print '0|' . $e->getMessage();
+        } catch (Exception $e) {
+            Mage::logException($e);
+            print '0|' . $e->getMessage();
+        }
+    }
+
+    /**
+     * AllPay returns POST variables to this action
+     */
+    public function resultAction() {
+        $isSuccess = FALSE;
+
+        try {
+            $oSession = $this->_getSession();
+            $oSession->unsAllpayRealOrderId();
+            $oSession->setQuoteId($oSession->getAllpayQuoteId(true));
+            $oSession->setLastSuccessQuoteId($oSession->getAllpaySuccessQuoteId(true));
+
+            $oPayment = new AllInOne();
+            $oPayment->HashKey = $this->_getConfigData('hash_key');
+            $oPayment->HashIV = $this->_getConfigData('hash_iv');
+            $oPayment->MerchantID = $this->_getConfigData('merchant_id');
+
+            $arFeedback = $oPayment->CheckOutFeedback();
+
+            if (sizeof($arFeedback) > 0) {
+                $isSuccess = $this->_processSale($arFeedback);
+            }
+        } catch (Mage_Core_Exception $e) {
+            $this->_getCheckout()->addError($e->getMessage());
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+
+        if ($isSuccess) {
+            $this->_redirect('checkout/onepage/success');
+        } else {
+            $this->_redirect('checkout/onepage/failure');
+        }
+    }
+
+    /**
+     * Process success response
+     */
+    protected function _processSale($request) {
+        $isSuccess = false;
+
+        try {
+            $szTradeNo = $request['TradeNo'];
+            $szMerchantTradeNo = ($this->_getConfigData('test_mode') ? str_replace($this->_getConfigData('test_order_prefix'), '', $request['MerchantTradeNo']) : $request['MerchantTradeNo']);
+            $szReturnCode = $request['RtnCode'];
+            $szPaymenttype = $request['PaymentType'];
+            
+            $oOrder = $this->_getOrder($szMerchantTradeNo);
+            // check transaction amount and currency
+            if ($this->_getConfigData('use_store_currency')) {
+                $dePrice = number_format($oOrder->getGrandTotal(), 0, '.', '');
+                $szCurrency = $oOrder->getOrderCurrencyCode();
+            } else {
+                $dePrice = number_format($oOrder->getBaseGrandTotal(), 0, '.', '');
+                $szCurrency = $oOrder->getBaseCurrencyCode();
+            }
+            // check transaction amount
+            if ((int) $dePrice != $request['TradeAmt']) {
+                Mage::throwException('Transaction amount doesn\'t match.');
+            }
+            // save transaction information
+            if ($request['RtnCode'] == '10100073' && $oOrder->getState() == Mage_Sales_Model_Order::STATE_NEW) {
+                $szPaymentType = $request['PaymentType'];
+                $szTradeDate = $request['TradeDate'];
+                $szBankCode = $request['PaymentNo'];
+                $szExpireDate = $request['ExpireDate'];
+                $szBarcode1 = $request['Barcode1'];
+                $szBarcode2 = $request['Barcode2'];
+                $szBarcode3 = $request['Barcode3'];
+
+                $szComment = sprintf(Mage::helper('barcode')->__('BARCODE Payment Information'), $szPaymentType, $szTradeDate, $szBankCode, $szExpireDate, $szBarcode1, $szBarcode2, $szBarcode3);
+
+                $oOrder->setState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT, TRUE, $szComment, FALSE);
+                $oOrder->save();
+            }
+            if ($request['RtnCode'] == '1' && $oOrder->getState() == Mage_Sales_Model_Order::STATE_PENDING_PAYMENT) {
+                $oOrder->getPayment()
+                        ->setTransactionId($szTradeNo)
+                        ->setLastTransId($szMerchantTradeNo)
+                        ->setCcStatus($szReturnCode)
+                        ->setCcType($szPaymenttype);
+                // 產生發票
+                if ($oOrder->canInvoice()) {
+                    $invoice = $oOrder->prepareInvoice();
+                    $invoice->register()->capture();
+                    $invoice->sendEmail(); //將發票E-mail給客戶
+                    Mage::getModel('core/resource_transaction')
+                            ->addObject($invoice)
+                            ->addObject($invoice->getOrder())
+                            ->save();
+                }
+                $oOrder->setState(Mage_Sales_Model_Order::STATE_PROCESSING, TRUE);
+                $oOrder->save();
+            }
+
+            $isSuccess = true;
+        } catch (Mage_Core_Exception $e) {
+            $this->_getCheckout()->addError($e->getMessage());
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+
+        return $isSuccess;
+    }
+
+}
